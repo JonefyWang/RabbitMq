620495ac6fae0b03a1905177371107a6ee5217ba
JonefyWang <wangyuanmimi@163.com>
2022-08-21 19:46:27 +0800

    优先级队列配置

--- rabbitmq-hello/src/main/java/com/wang/rabbit/one/Product.java
+++ rabbitmq-hello/src/main/java/com/wang/rabbit/one/Product.java
@@ -1,11 +1,14 @@
 package com.wang.rabbit.one;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -37,8 +40,20 @@ public class Product {
              * 4.是否自动删除 最后一个消费者端开连接以后 该队列是否自动删除 true 自动删除
              * 5.其他参数
              */
-            channel.queueDeclare(QUEUE_NAME,false,false,false,null);
-            String message = "hello world";
+            Map<String,Object> arguments = new HashMap<>();
+            //官方允许是0-255之间  此处设置为10  允许优化级范围0-10  不要设置过大浪费CPU与内存
+            arguments.put("x-max-priority",10);
+            channel.queueDeclare(QUEUE_NAME,true,false,false,arguments);
+
+            for (int i = 1; i < 11; i++) {
+                String message = "info" + i;
+                if (i == 5){
+                    AMQP.BasicProperties properties = new AMQP.BasicProperties().builder().priority(5).build();
+                    channel.basicPublish("",QUEUE_NAME,properties,message.getBytes());
+                }else {
+                    channel.basicPublish("",QUEUE_NAME,null,message.getBytes());
+                }
+            }
             /**
              * 发送一个消息
              * 1.发送到那个交换机
@@ -46,7 +61,7 @@ public class Product {
              * 3.其他的参数信息
              * 4.发送消息的消息体
              */
-            channel.basicPublish("",QUEUE_NAME,null,message.getBytes());
+            //channel.basicPublish("",QUEUE_NAME,null,message.getBytes());
             System.out.println("消息队列发送完毕！");
         }
     }
